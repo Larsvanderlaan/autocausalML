bd65e820557a0541c84f52e152b4e77542237c65
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-08 10:50:56 -0700

    hi

--- simScripts/test.R
+++ simScripts/test.R
@@ -1,3 +1,3 @@
 devtools::install_github("tlverse/hal9001", ref = "Larsvanderlaan-formula-patch")
-devtools::install_github("tlverse/sl3", ref = "devel")
+devtools::install_github("tlverse/sl3", ref = "develVersionChangeLars")
 devtools::install_github("Larsvanderlaan/autocausalML")
