04c1bf70168aab5214cec5d44d9366fe1a3bd833
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-13 13:39:02 -0700

    wait

--- simScripts/simsComplex.sh
+++ simScripts/simsComplex.sh
@@ -7,6 +7,6 @@ for n in 1000 #500 1000 2500 5000
 do
   for const in 3 #5 8
   do
-    sbatch  --export=n=$n,const=$const simsCATE_Complex.sbatch
+    sbatch  --export=n=$n,const=$const ~/sieveSims/simsComplex.sbatch
   done
 done
