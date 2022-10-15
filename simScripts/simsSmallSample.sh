ff72b1352ef5e2847ea7e74019e71557c04c6553
Lars van der Laan <vanderlaanlars@yahoo.com>
2022-10-14 18:33:51 -0700

    hi

--- simScripts/simsSmallSample.sh
+++ simScripts/simsSmallSample.sh
@@ -1,8 +1,8 @@
 #!/bin/usr/env bash
 filename="simsCATE_xgboost"
 nsims=2500
-export R_LIBS=~/Rlibs
-export R_LIBS_USER=~/Rlibs
+export R_LIBS=~/Rlibs2
+export R_LIBS_USER=~/Rlibs2
 for n in 50 100 150 200 300 500
 do
   for const in 4 7 1
